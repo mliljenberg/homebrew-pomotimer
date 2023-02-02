0643e790f80bcbddd38a97d299a4d8d43b7467a0
Marcus <mliljenberg@gmail.com>
2023-02-02 11:16:46 +0100

    updates sha

--- Formula/pomotimer.rb
+++ Formula/pomotimer.rb
@@ -4,7 +4,7 @@
 class Pomotimer < Formula
   desc "A simple cli for a pomodoro timer built in rust"
   homepage "https://github.com/mliljenberg/pomotimer"
-  url "https://github.com/mliljenberg/pomotimer/releases/download/v0.1.0/pomotimer-mac.tar.gz"
+  url "https://github.com/mliljenberg/pomotimer/releases/download/v0.1.1/pomotimer-mac.tar.gz"
   sha256 "46b11a22c75bee5b24db2a655737684ec1b48c7c2772b4ba02fad8703513bd6f"
   version "0.1.0"
 
